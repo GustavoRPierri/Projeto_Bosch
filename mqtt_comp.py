2b6917c6f348e2f00453e285a1bcc6feb6424c58
GustavoRPierri <gustavoromanopierri@hotmail.com>
2020-09-19 17:18:45 -0300

    Add sites auxiliares

--- mqtt_comp.py
+++ mqtt_comp.py
@@ -1,4 +1,28 @@
 #!pip install paho-mqtt
 
 import paho.mqtt.client as mqtt
+import time
 
+def on_connect(client, userdata, flags, rc): # FUNÇÃO DE CALL BACK, ELA IRÁ NOS RETORNAR SE A CONEXÃO COM O BROKER ESTA VIAVEL OU NÃO.
+    if rc ==0: # 'rc' = RESULTADO DA CONEXÃO, RETORNA UM VALOR CORRESPONDENTE AO STATUS DA CONEXÃO ATUAL.
+        print('connected OK') # 'rc' = 0 (C)
+    else:
+        print('Bad connection, Returned code =',rc)
+
+client = mqtt.Client("Computador") # CRIA UM NOME PARA O CLIENTE QUE ACESSAR O BROKEN PELO PYTHON. 
+
+client.on_connect=on_connect
+
+cliente.loop_start()
+
+client.connect("192.168.15.3", 1883, 60) # CONECTA AO BROKER.
+# '192.168.15.3' = endereço IP do broker remoto.
+# '1883' = porta de rede do host.
+# '60' = período máximo em segundos.
+
+cliente.publish("topico_teste","Helow Word")
+time.sleep(4)
+
+cliente.loop_stop()
+
+cliente.disconnect() # DISCONECTA DO BROKER.
