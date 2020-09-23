297dc35e1b1bc0bf8143bbb6143fcd7689fc1eb2
GustavoRPierri <gustavoromanopierri@hotmail.com>
2020-09-22 23:47:45 -0300

    Arrumando codigo de mqtt_comp

--- mqtt_comp.py
+++ mqtt_comp.py
@@ -1,7 +1,7 @@
 #!pip install paho-mqtt
-
 import paho.mqtt.client as mqtt
-import time
+
+L = []
 
 def on_connect(client, userdata, flags, rc): # FUNÇÃO DE CALL BACK, ELA IRÁ NOS RETORNAR SE A CONEXÃO COM O BROKER ESTA VIAVEL OU NÃO.
     if rc ==0: # 'rc' = RESULTADO DA CONEXÃO, RETORNA UM VALOR CORRESPONDENTE AO STATUS DA CONEXÃO ATUAL.
@@ -9,21 +9,35 @@ def on_connect(client, userdata, flags, rc): # FUNÇÃO DE CALL BACK, ELA IRÁ N
     else:
         print('Bad connection, Returned code =',rc)
 
+def on_message(client, userdata, message):
+    msg = str(message.payload.decode("utf-8"))
+    L.append(msg)   
+    return msg
+
+########################################################
+
 client = mqtt.Client("Computador") # CRIA UM NOME PARA O CLIENTE QUE ACESSAR O BROKEN PELO PYTHON. 
 
 client.on_connect=on_connect
 
-cliente.loop_start()
+client.on_message=on_message
 
-client.connect("192.168.15.3", 1883, 60) # CONECTA AO BROKER.
+client.connect("192.168.15.3") # CONECTA AO BROKER.
 # '192.168.15.3' = endereço IP do broker remoto.
 # '1883' = porta de rede do host.
 # '60' = período máximo em segundos.
 
-cliente.publish("topico_teste","Helow Word")
-time.sleep(4)
+client.loop_start()
 
-cliente.loop_stop()
+client.subscribe("topico_teste")
+client.publish("topico_teste","Helow Word")
+print(L)
+L = []
 
-cliente.disconnect() # DISCONECTA DO BROKER.
+#########################################################
 
+client.loop_stop()
+
+client.disconnect() # DISCONECTA DO BROKER.
+
+############################################################################################
